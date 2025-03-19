5db3e1fd8f15c94552859da7083000455f34f562
Farrux <Farrux@gmail.com>
2025-03-19 09:51:03 +0500

    implement product modal functionality with state management

--- src/hooks/Product.jsx
+++ src/hooks/Product.jsx
@@ -1,24 +1,17 @@
 import { useEffect, useState } from "react";
 
 export function Product(p, setShowModal) {
-  const [localProduct, setLocalProduct] = useState(p.product);
+  const { title, thumbnail, id, description } = p.product;
 
-  useEffect(() => {
-    if (p.product) {
-      setLocalProduct(p.product);
-    }
-  }, [p.product]);
-
-  if (!localProduct) return null;
   return (
     <div className="modal__box">
       <div className="card lg:card-side bg-base-100 shadow-sm">
         <figure>
-          <img src={localProduct.thumbnail} alt="image of product" />
+          <img src={thumbnail} alt="image of product" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{localProduct.title}</h2>
-          <p>{localProduct.description}</p>
+          <h2 className="card-title">{title}</h2>
+          <p>{description}</p>
           <div className="card-actions justify-end">
             <button
               onClick={() => setShowModal(false)}
